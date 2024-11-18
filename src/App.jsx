9e896c1340d1504c8746eb7e8816cb50d20913f7
Sensy-gmn <alexandre26goumain@gmail.com>
2024-11-18 14:43:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Title from "./Components/Templates/Title";
+
+function App() {
+    return (
+        <div className="container mx-auto">
+            <Title title="hello" />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/Templates/Title.jsx
@@ -0,0 +1,3 @@
+export default function Title({ title }) {
+    return <h1 className="text-3xl font-bold text-blue-600">{title}</h1>;
+}
