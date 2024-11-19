d9c54def48d06b6ec15292e90670867b98807076
Sensy-gmn <alexandre26goumain@gmail.com>
2024-11-19 11:44:18 +0100

    State for backgroundColor

--- src/Components/ResponsiveBackground.jsx
+++ src/Components/ResponsiveBackground.jsx
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
+
 export default function ResponsiveBackground() {
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
+    const [widthBackgroundColor, setWidthBackgroundColor] = useState("");
+    const [heightBackgroundColor, setHeightBackgroundColor] = useState("");
 
     const handleResize = () => {
-        setWidth(window.innerWidth);
-        setHeight(window.innerHeight);
+        const newWidth = window.innerWidth;
+        const newHeight = window.innerHeight;
+
+        setWidth(newWidth);
+        setHeight(newHeight);
+
+        setWidthBackgroundColor(
+            newWidth >= 800 ? "bg-[#ADD8E6]" : "bg-[#FFC0CB]"
+        );
+        setHeightBackgroundColor(
+            newHeight >= 600 ? "bg-green-800" : "bg-yellow-500"
+        );
     };
 
     useEffect(() => {
@@ -17,11 +30,6 @@ export default function ResponsiveBackground() {
         };
     }, []);
 
-    const widthBackgroundColor = width >= 800 ? "bg-[#ADD8E6]" : "bg-[#FFC0CB]";
-
-    const heightBackgroundColor =
-        height >= 600 ? "bg-green-800" : "bg-yellow-500";
-
     return (
         <>
             <div className={`${widthBackgroundColor} p-4`}>
@@ -29,6 +37,7 @@ export default function ResponsiveBackground() {
                     La largeur de la fenêtre est de {width}px
                 </p>
             </div>
+
             <div className={`${heightBackgroundColor} p-4`}>
                 <p className="text-5xl">
                     La hauteur de la fenêtre est de {height}px
